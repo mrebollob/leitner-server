c77d3a06d07f45db61bf26c6cd64cfd024e35d73
Manuel Rebollo <mrebollob@gmail.com>
2019-02-18 18:44:14 +0100

    Update bot config

--- bot.js
+++ bot.js
@@ -1,16 +1,7 @@
 const token = process.env.TOKEN;
 
 const TelegramBot = require('node-telegram-bot-api');
-let bot;
-
-if (process.env.NODE_ENV === 'production') {
-    bot = new TelegramBot(token);
-    bot.setWebHook(process.env.HEROKU_URL + bot.token);
-} else {
-    bot = new TelegramBot(token, {
-        polling: true
-    });
-}
+const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
