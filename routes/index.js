20bd04029d5e566225e48a31f8e73ac3fffbc80d
Manuel Rebollo <mrebollob@gmail.com>
2019-02-20 11:51:00 +0100

    Remove bot

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var bot = require('../bot');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-/*
-router.post('/' + bot.token, function(req, res) {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-*/
 
 module.exports = router;
